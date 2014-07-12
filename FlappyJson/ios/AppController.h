d116b44ef7db8b525eb0b152cf7fa8bff2753f40
Jsonmess <Jsonmess@163.com>
2014-07-12 18:14:43 +0800

    Initial Commit

---
+++ FlappyJson/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  FlappyJsonAppController.h
+//  FlappyJson
+//
+//  Created by Json on 14-7-12.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ FlappyJson/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FlappyJsonAppController.h
+//  FlappyJson
+//
+//  Created by Json on 14-7-12.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
